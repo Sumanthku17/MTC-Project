c3bc27cbb12f50bff102790178ae84b08b40e1c7
Sumanth Kumar Shetty C <sumanthkumar.shettyc@mediakind.com>
2022-07-10 18:32:56 +0530

    Initial Setup

---
+++ MTC Project/Models/Employees.cs
@@ -0,0 +1,13 @@
+﻿namespace MTC_Project.Models
+{
+    public class Employees
+    {
+        public int EmployeeId { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string Department { get; set; }
+
+        public string PhotoFileName { get; set; }
+    }
+}
